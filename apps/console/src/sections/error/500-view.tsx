f87f41c5f98625f7c45d7a97950a1b6717184292
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-12-21 21:28:02 +0530

    feat: adds console ui

--- apps/console/src/sections/error/500-view.tsx
+++ apps/console/src/sections/error/500-view.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import {m} from 'framer-motion';
+import { m } from 'framer-motion';
 
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-import {RouterLink} from '@karma-erp/utils/routes/components';
+import { RouterLink } from '@gc-broadcast-web/utils/routes/components';
 
-import CompactLayout from '@karma-erp/utils/layouts/compact';
-import {SeverErrorIllustration} from '@karma-erp/utils/assets/illustrations';
+import CompactLayout from '@gc-broadcast-web/utils/layouts/compact';
+import { SeverErrorIllustration } from '@gc-broadcast-web/utils/assets/illustrations';
 
-import {MotionContainer, varBounce} from '@karma-erp/components/animate';
+import { MotionContainer, varBounce } from '@gc-broadcast-web/components/animate';
 
 // ----------------------------------------------------------------------
 
@@ -19,19 +19,19 @@ export default function Page500() {
     <CompactLayout>
       <MotionContainer>
         <m.div variants={varBounce().in}>
-          <Typography variant="h3" sx={{mb: 2}}>
+          <Typography variant="h3" sx={{ mb: 2 }}>
             500 Internal Server Error
           </Typography>
         </m.div>
 
         <m.div variants={varBounce().in}>
-          <Typography sx={{color: 'text.secondary'}}>
+          <Typography sx={{ color: 'text.secondary' }}>
             There was an error, please try again later.
           </Typography>
         </m.div>
 
         <m.div variants={varBounce().in}>
-          <SeverErrorIllustration sx={{height: 260, my: {xs: 5, sm: 10}}}/>
+          <SeverErrorIllustration sx={{ height: 260, my: { xs: 5, sm: 10 } }} />
         </m.div>
 
         <Button component={RouterLink} href="/" size="large" variant="contained">
